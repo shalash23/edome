5a1318329d23ce705ffc63f1d516cb6fc78b1dbb
shalash23 <shalashmohamed232@outlook.com>
2023-04-30 16:04:13 +0300

    adjusted the text inside of the card

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -18,7 +18,7 @@ const ProductsList = ({ products }: Props) => {
   return (
     <div className="mt-10 sm:ml-10 lg:col-span-2">
       <ul className="md:pt-7 space-y-12 sm:grid sm:grid-cols-1 md:grid-cols-3 xl:grid-cols-3 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:gap-x-8">
-        {products.map(({ images, name, variants, reference, slug }, key: number) => {
+        {products.map(({ images, name, variants, reference, slug, description }, key: number) => {
           const img = _.first(images)?.url;
           const code = _.first(variants)?.code;
           return (
@@ -30,7 +30,7 @@ const ProductsList = ({ products }: Props) => {
                   </div>
                   <div className="text-base leading-6 font-medium space-y-1 justify-self-start h-full">
                     <h3>{name}</h3>
-                    <p className="text-gray-700 text-sm">{reference}</p>
+                    <p className="text-gray-700 text-sm truncate">{description}</p>
                   </div>
                   <div className="justify-self-end mt-5">
                     <ul className="flex justify-between space-x-1 items-center">
